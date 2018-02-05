0f4e9299d9c5e1eed33a055525c49f8d0f8ee4cf
Marlon Rugama <MarlonRugama@ac.c4q.nyc>
2018-02-04 19:46:16 -0500

    Added Services and Models

--- Unit5GroupProjectTwo/Services/PostService.swift
+++ Unit5GroupProjectTwo/Services/PostService.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseDatabase
+
+struct PostService {
+    private init() {
+        dbRef = Database.database().reference()
+        postRef = dbRef.child("posts")
+        guard let cUser = Auth.auth().currentUser else {print("No current user available"); return}
+        currentUser = cUser
+    }
+    
+    static let manager = PostService()
+    private var dbRef: DatabaseReference!
+    private var postRef: DatabaseReference!
+    private var currentUser: User!
+    
+    public func getDB()-> DatabaseReference { return dbRef }
+    public func getPosts()-> [Post] {
+        var posts = [Post]()
+        postRef.observe(.value) { (dataSnapShop) in
+            for post in dataSnapShop.children {
+                let post = Post(snapShot: post as! DataSnapshot)
+                posts.append(post)
+            }
+        }
+        return posts
+    }
+    public func getUserPosts(from user: User) -> [Post] {
+        var posts = [Post]()
+        let userPosts = postRef.child("posts").queryEqual(toValue: user, childKey: "user")
+        userPosts.observe(.value) { (dataSnapShot) in
+            let postsOnline = dataSnapShot.children
+            for post in postsOnline {
+                let post = Post(snapShot: post as! DataSnapshot)
+                posts.append(post)
+            }
+        }
+        return posts
+    }
+    
+    public func saveNewPost(content: String, title: String) {
+        let newPost = postRef.childByAutoId()
+        let post = Post(ref: newPost, user: currentUser, postContent: content, postTitle: title)
+        newPost.setValue(post.toAnyObject())
+    }
+}
